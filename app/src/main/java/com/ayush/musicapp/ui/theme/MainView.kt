55bf776f0cf0ee64ad377b6f9ae2734d8ab07d5a
Ayushrai2005 <shweetarai@gmail.com>
2024-02-28 23:56:26 +0530

    Makes the Drawer items Clickable . (Setting up Navigation and ViewModel)

--- app/src/main/java/com/ayush/musicapp/ui/theme/MainView.kt
+++ app/src/main/java/com/ayush/musicapp/ui/theme/MainView.kt
@@ -1,11 +1,19 @@
 package com.ayush.musicapp.ui.theme
 
+import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.ScaffoldState
@@ -17,6 +25,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -25,14 +35,29 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.LineHeightStyle.Alignment.*
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.ayush.musicapp.MainViewModel
+import com.ayush.musicapp.R
 import com.ayush.musicapp.Screen
 import com.ayush.musicapp.Screen.DrawerScreen.Account.title
 import com.ayush.musicapp.screensInDrawer
@@ -40,12 +65,12 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
-@Preview(showBackground = true)
 @Composable
 fun MainView(){
 
     val scaffoldState : ScaffoldState = rememberScaffoldState()
     val scope : CoroutineScope = rememberCoroutineScope()
+    val viewModel : MainViewModel = viewModel()
 
 
     //Use to find out on which "view" we currently are
@@ -53,9 +78,13 @@ fun MainView(){
     val  newBackStackEntry  by controller.currentBackStackEntryAsState()
     val currentRoute = newBackStackEntry?.destination?.route
 
+    val currentScreen = remember {
+        viewModel.currentScreen.value
+    }
+
     val title = remember {
         // TODO change that to currentScreen.title
-        mutableStateOf("")
+        mutableStateOf(currentScreen.title)
     }
 
     Scaffold(
@@ -94,8 +123,7 @@ fun MainView(){
         }
 
     ) {
-        Text(text = "Home" , modifier = Modifier.padding(it))
-        
+            Navigation(navController = controller, viewModel = viewModel, pd = it)
     }
 }
 
@@ -104,8 +132,8 @@ fun DrawerItem(
     selected : Boolean,
     item: Screen.DrawerScreen,
     onDrawerItemClicked:() -> Unit
-){
-    val background = if(selected) Color.DarkGray else Color.White
+) {
+    val background = if (selected) Color.DarkGray else Color.White
 
     Row(
         Modifier
@@ -115,14 +143,31 @@ fun DrawerItem(
             .clickable {
                 onDrawerItemClicked()
             }
-    ){
+    ) {
         androidx.compose.material.Icon(
             painter = painterResource(id = item.icon),
             contentDescription = item.dTitle,
-            Modifier.padding(end = 8.dp , top = 4.dp)
+            Modifier.padding(end = 8.dp, top = 4.dp)
         )
         Text(text = item.dTitle, style = MaterialTheme.typography.titleMedium)
+    }
+}
+
+@Composable
+fun Navigation(navController: NavController , viewModel: MainViewModel , pd : PaddingValues){
+    
+    NavHost(navController = navController as NavHostController,
+        startDestination = Screen.DrawerScreen.AddAccount.route,
+        modifier = Modifier.padding(pd) ){
+        // Only two composable beacuse for
+
+        composable(Screen.DrawerScreen.AddAccount.route){
+
+        }
+        composable(Screen.DrawerScreen.subscription.route){
+
+        }
 
     }
-
+    
 }
